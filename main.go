421ddbe48e1547718722ef84e60bb3f0659bbccd
Benjamin Kohler <bkohler@codenomad.com>
2025-04-07 16:58:41 +0200

    Push current state

--- main.go
+++ main.go
@@ -41,6 +41,29 @@ func main() {
 	nerdFlag := flag.Bool("n", false, "Nerd joke")
 	sportFlag := flag.Bool("s", false, "Sport joke")
 	runFlag := flag.Bool("r", false, "Running joke")
+
+	// Additional joke category flags
+	dadFlag := flag.Bool("d", false, "Dad joke")
+	animalFlag := flag.Bool("a", false, "Animal joke")
+	techFlag := flag.Bool("t", false, "Tech joke")
+	programmerFlag := flag.Bool("p", false, "Programmer joke")
+	mathFlag := flag.Bool("m", false, "Math joke")
+	scienceFlag := flag.Bool("c", false, "Science joke")
+	foodFlag := flag.Bool("f", false, "Food joke")
+	doctorFlag := flag.Bool("o", false, "Doctor joke")
+	lawyerFlag := flag.Bool("l", false, "Lawyer joke")
+	politicalFlag := flag.Bool("q", false, "Political joke")
+	blondeFlag := flag.Bool("b", false, "Blonde joke")
+	knockFlag := flag.Bool("k", false, "Knock-knock joke")
+	schoolFlag := flag.Bool("s2", false, "School joke")
+	musicFlag := flag.Bool("u", false, "Music joke")
+	movieFlag := flag.Bool("v", false, "Movie joke")
+	historyFlag := flag.Bool("h2", false, "History joke")
+	relationshipFlag := flag.Bool("r2", false, "Relationship joke")
+	workFlag := flag.Bool("w", false, "Work joke")
+	travelFlag := flag.Bool("j", false, "Travel joke")
+	punFlag := flag.Bool("n2", false, "Pun joke")
+
 	flag.Parse()
 
 	if *helpFlag {
@@ -51,6 +74,26 @@ func main() {
 		fmt.Println("  -n        Nerd joke")
 		fmt.Println("  -s        Sport joke")
 		fmt.Println("  -r        Running joke")
+		fmt.Println("  -d        Dad joke")
+		fmt.Println("  -a        Animal joke")
+		fmt.Println("  -t        Tech joke")
+		fmt.Println("  -p        Programmer joke")
+		fmt.Println("  -m        Math joke")
+		fmt.Println("  -c        Science joke")
+		fmt.Println("  -f        Food joke")
+		fmt.Println("  -o        Doctor joke")
+		fmt.Println("  -l        Lawyer joke")
+		fmt.Println("  -q        Political joke")
+		fmt.Println("  -b        Blonde joke")
+		fmt.Println("  -k        Knock-knock joke")
+		fmt.Println("  -s2       School joke")
+		fmt.Println("  -u        Music joke")
+		fmt.Println("  -v        Movie joke")
+		fmt.Println("  -h2       History joke")
+		fmt.Println("  -r2       Relationship joke")
+		fmt.Println("  -w        Work joke")
+		fmt.Println("  -j        Travel joke")
+		fmt.Println("  -n2       Pun joke")
 		os.Exit(0)
 	}
 
@@ -67,6 +110,66 @@ func main() {
 	}
 	if *runFlag {
 		categories = append(categories, "running")
+	}
+	if *dadFlag {
+		categories = append(categories, "dad")
+	}
+	if *animalFlag {
+		categories = append(categories, "animal")
+	}
+	if *techFlag {
+		categories = append(categories, "tech")
+	}
+	if *programmerFlag {
+		categories = append(categories, "programmer")
+	}
+	if *mathFlag {
+		categories = append(categories, "math")
+	}
+	if *scienceFlag {
+		categories = append(categories, "science")
+	}
+	if *foodFlag {
+		categories = append(categories, "food")
+	}
+	if *doctorFlag {
+		categories = append(categories, "doctor")
+	}
+	if *lawyerFlag {
+		categories = append(categories, "lawyer")
+	}
+	if *politicalFlag {
+		categories = append(categories, "political")
+	}
+	if *blondeFlag {
+		categories = append(categories, "blonde")
+	}
+	if *knockFlag {
+		categories = append(categories, "knock-knock")
+	}
+	if *schoolFlag {
+		categories = append(categories, "school")
+	}
+	if *musicFlag {
+		categories = append(categories, "music")
+	}
+	if *movieFlag {
+		categories = append(categories, "movie")
+	}
+	if *historyFlag {
+		categories = append(categories, "history")
+	}
+	if *relationshipFlag {
+		categories = append(categories, "relationship")
+	}
+	if *workFlag {
+		categories = append(categories, "work")
+	}
+	if *travelFlag {
+		categories = append(categories, "travel")
+	}
+	if *punFlag {
+		categories = append(categories, "pun")
 	}
 
 	// Build prompt
